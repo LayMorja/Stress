e80b441019bed353f9e48f18e9f5083ed7600dcc
LayMorja <evil-corporaty@yandex.ru>
2022-04-27 01:30:40 +0300

    First comit

---
+++ js/app.js
@@ -0,0 +1,491 @@
+const DATA = [
+   {
+      question: 'Недоверчивость к окружающим',
+      answers: [
+         {
+            id: '0',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '1',
+            value: 'Нет',
+            index: 0,
+         },
+      ]
+   },   
+   {
+      question: 'Плохое настроение',
+      answers: [
+         {
+            id: '2',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '3',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Ощущение грусти, боли',
+      answers: [
+         {
+            id: '4',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '5',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Частая вспыльчивость',
+      answers: [
+         {
+            id: '6',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '7',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Неудовлетворенность жизнью',
+      answers: [
+         {
+            id: '8',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '9',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Чувство одиночества',
+      answers: [
+         {
+            id: '10',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '11',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Потеря интереса к жизни',
+      answers: [
+         {
+            id: '12',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '13',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Падение самооценки',
+      answers: [
+         {
+            id: '14',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '15',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Преобладание негативных мыслей',
+      answers: [
+         {
+            id: '16',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '17',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Снижение концентрации внимания  ',
+      answers: [
+         {
+            id: '18',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '19',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Отвлекаемость',
+      answers: [
+         {
+            id: '20',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '21',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Неопределенность в принятии решений',
+      answers: [
+         {
+            id: '22',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '23',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Снятся кошмары',
+      answers: [
+         {
+            id: '24',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '25',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Не хочется что-либо делать',
+      answers: [
+         {
+            id: '26',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '27',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Переживание',
+      answers: [
+         {
+            id: '28',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '29',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Спешишь с необоснованными решениями',
+      answers: [
+         {
+            id: '30',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '31',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Ошибки при выполнении обычных действий',
+      answers: [
+         {
+            id: '32',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '33',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Частые конфликты в школе или в семье',
+      answers: [
+         {
+            id: '34',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '35',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Постоянная нехватка времени',
+      answers: [
+         {
+            id: '36',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '37',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Уменьшение внимания близким',
+      answers: [
+         {
+            id: '38',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '39',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Не ухаживаешь за собой',
+      answers: [
+         {
+            id: '40',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '41',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Выполняешь меньше заданий, чем обычно',
+      answers: [
+         {
+            id: '42',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '43',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Головные боли',
+      answers: [
+         {
+            id: '44',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '45',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Быстрее утомляешься, чем обычно',
+      answers: [
+         {
+            id: '46',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '47',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+   {
+      question: 'Дрожь в руках',
+      answers: [
+         {
+            id: '48',
+            value: 'Да',
+            index: 1,
+         },
+         {
+            id: '49',
+            value: 'Нет',
+            index: 0,
+         }
+      ]
+   },  
+];
+
+
+let localResults = {};
+let indexCount = 0;
+const quiz = document.getElementById('quiz');
+const questions = document.getElementById('questions');
+const indicator = document.getElementById('indicator');
+const results = document.getElementById('results');
+const btnNext = document.getElementById('btn-next');
+const btnRestart = document.getElementById('btn-restart');
+
+
+
+const renderQuestions = (index) => {
+   renderIndicator(index + 1);
+
+   questions.dataset.currentStep = index;
+
+   const renderAnswers = () => DATA[index].answers
+      .map((answer) => `
+                  <li>
+                     <label>
+                        <input class="answer__input" type="radio" name="${index}" value="${answer.id}">
+                        ${answer.value}
+                     </label>
+                  </li>
+      `)
+      .join('');
+   questions.innerHTML = `
+      <div class="quiz__items item-quiz">
+         <h4 class="item-quiz__title">${DATA[index].question}</h4>
+         <ul class="item-quiz__answers">${renderAnswers()}</ul>
+      </div>
+   `;
+};
+const renderResults = (indexCount) => {
+   console.log(indexCount);
+   rightAnswer = (indexCount / DATA.length) * 100;
+   console.log(rightAnswer);
+   content = ``;
+   if (rightAnswer <= 20) {
+      content += `
+            <h2>Результат теста:</h2>
+            <p class="result__text">Стресс почти или полностью отсутствует.</p>`;      
+   } else if (rightAnswer >= 21 && rightAnswer <= 40) {      
+      content += `
+            <h2>Результат теста:</h2>
+            <p class="result__text">Стресс умеренный.<br>
+            Что можно сделать? <br>
+            1. планировать своё время <br>
+            2. отдыхать</p>`;     
+   } else if (rightAnswer >= 41 && rightAnswer <= 60) {      
+      content += `
+            <h2>Результат теста:</h2>
+            <p class="result__text">Ваше состояние довольно-таки напряжено. <br>
+            Что можно сделать? Устройте себе полноценный отдых, не занимайтесь слишком трудоёмкий процессом в ближайшее время. </p>`;     
+   } else if (rightAnswer >= 61 && rightAnswer <= 80) {      
+      content += `
+            <h2>Результат теста:</h2>
+            <p class="result__text">Стресс сильный.<br>
+            Что можно сделать? Необходимо проанализировать свою ситуацию, в который Вы находитесь, чтобы избежать наиболее трудную стадию стресса, и исправить её. Если не получится самостоятельно, то лучше обратиться к специалисту. </p>`;     
+   } else {      
+      content += `
+            <h2>Результат теста:</h2>
+            <p class="result__text">Наиболее опасная стадия стресса.<br>
+            Что можно сделать? Необходимо обратиться к специалисту.</p>`;     
+   }   
+   results.innerHTML = content;
+};
+
+const renderIndicator = (currentStep) => {
+   indicator.innerHTML = `${currentStep} / ${DATA.length}`;
+};
+
+quiz.addEventListener('change', (event) => {
+   if (event.target.classList.contains('answer__input')) {
+      localResults[event.target.name] = event.target.value;
+      if (event.target.value % 2 == 0) {
+         indexCount += 1;
+      }
+      btnNext.disabled = false;
+   }
+});
+
+quiz.addEventListener('click', (event) => {
+   if (event.target.classList.contains('btn__next')) {
+      const nextQuestionID = Number(questions.dataset.currentStep) + 1;     
+      if(DATA.length === nextQuestionID) {
+         questions.classList.add('_hidden');
+         indicator.classList.add('_hidden');
+         results.classList.add('_visible');
+         btnNext.classList.add('_hidden');
+         btnRestart.classList.add('_visible');
+         renderResults(indexCount);
+      } else {
+         renderQuestions(nextQuestionID);
+      }
+
+      btnNext.disabled = true;
+   }
+   if (event.target.classList.contains('btn__restart')) {
+      localResults = {};
+      results.innerHTML = '';
+      questions.classList.remove('_hidden');
+      indicator.classList.remove('_hidden');
+      results.classList.remove('_visible');
+      btnNext.classList.remove('_hidden');
+      btnRestart.classList.remove('_visible');
+
+      renderQuestions(0);
+   }
+});
+
+renderQuestions(0);
